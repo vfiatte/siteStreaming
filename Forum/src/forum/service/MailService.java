b7ef1dd7b5230558fc1eb80363e1373eed34ed71
vfiatte <valentin.fiatte@gmail.com>
2016-02-25 14:38:47 +0100

    Modification sur les appels des DAO dans les services

---
+++ Forum/src/forum/service/MailService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forum.service;
+
+/**
+ *
+ * @author admin
+ */
+public interface MailService {
+    public void Mail(String Emeteur, String Dest, String Titre, String Message);
+    
+
+    public void Click();
+      
+    
+}
